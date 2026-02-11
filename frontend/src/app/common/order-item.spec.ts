7105a0ec36f488e93c3d93b5ee7c5c54d500e2ce
hamzaatlili <162675281+hamzaatlili@users.noreply.github.com>
2026-02-11 09:41:25 +0100

    Update order-item.spec.ts

--- frontend/src/app/common/order-item.spec.ts
+++ frontend/src/app/common/order-item.spec.ts
@@ -1,7 +1,19 @@
-import { OrderItem } from './order-item';
+/*import { OrderItem } from './order-item';
 
 describe('OrderItem', () => {
   it('should create an instance', () => {
     expect(new OrderItem()).toBeTruthy();
+  });
+});
+*/
+import { OrderItem } from './order-item';
+import { CartItem } from './cart-item';
+import { Product } from './product';
+
+describe('OrderItem', () => {
+  it('should create OrderItem', () => {
+    const mockProduct = new Product('1', 'Sample Product', 100);
+    const cartItem = new CartItem(mockProduct);
+    expect(new OrderItem(cartItem)).toBeTruthy();
   });
 });
